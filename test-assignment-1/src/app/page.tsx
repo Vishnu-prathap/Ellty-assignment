df593cf1c420f8da1bd9867ef337593b61fc8857
Vishnu Prathap JRVP <85738017+Vishnu-prathap@users.noreply.github.com>
2024-07-20 00:28:08 +0530

    CHORE: Changed font to Montserrat, added shadow effect and verified paddings of all elements

--- test-assignment-1/src/app/page.tsx
+++ test-assignment-1/src/app/page.tsx
@@ -1,31 +1,37 @@
 "use client";
 export default function Home() {
-  const myFunction = () => {
-    return 1;
-  };
+ 
   return (
-    <div className=' flex flex-col items-center justify-center'>
-      <div className='border-2 p-10 gap-y-4 flex flex-col min-w-[370px]'>
-        <div className='flex flex-row  min-h-[42px] justify-between items-center'>
+    <div className=' flex flex-col items-center justify-center pt-5'>
+      <div className='border-solid rounded-md border-t-1 shadow-lg py-[10px] gap-y-4 flex flex-col min-w-[370px] justify-center'>
+        <div className='flex flex-row justify-between items-center py-[8px] pr-[15px] pl-[22px] '>
           <p>All pages</p>
-          <input type='checkbox' className=' text-blue-600 rounded-sm' />
+          <input type='checkbox' className=' text-blue-600 rounded-sm hover:' />
         </div>
-        <hr className='text-black w-[340px] decoration-5' />
+        <div className="flex flex-row justify-between items-center py-[8px] pr-[15px] pl-[22px]">
+          <hr className='text-black w-[340px] border-t-1' />
+        </div>
 
         {[1, 2, 3, 4].map((item, index) => {
           return (
             <div
-              className='flex flex-row  max-h-[164px] items-center justify-between'
+              className='flex flex-row  max-h-[164px] items-center justify-between py-[8px] pr-[15px] pl-[22px]'
               key={index}>
               <p>Page {item}</p>
               <input type='checkbox' className=' text-blue-600 rounded-sm' />
             </div>
           );
         })}
-        <hr className='text-black w-[340px] decoration-5' />
-
-        <button className="bg-amber-400  min-h-[40px] hover:bg-amber-300">Done</button>
-
+        <div className="flex flex-row justify-between items-center py-[8px] pr-[15px] pl-[22px]">
+          <hr className='text-black w-[340px] border-t-1' />
+        </div>
+        
+          <div className="py-[10px] px-[20px]">
+            <button className='bg-amber-400 w-[340px] min-h-[40px] rounded-[4px] hover:bg-amber-300'>
+              Done
+            </button>
+          </div>
+        
       </div>
     </div>
   );
